8628d6955640048a4074d16658512e7ee9955117
okoba <taakiwami@gmail.com>
2026-03-14 16:37:28 +0900

    全体的なブラッシュアップ

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
+// モーダルに必要な要素取得
 const reformBtn = document.getElementById('reform-btn');
 const reformDialog = document.getElementById('reform-dialog');
+const reformClose = document.getElementById('reform-close');
+
+const mypageBtn = document.getElementById('mypage-btn');
+const mypageDialog = document.getElementById('mypage-dialog');
+const mypageClose = document.getElementById('mypage-close');
+
 
 const gyozaBtn = document.getElementById('gyoza-btn');
 const gyozaDialog = document.getElementById('gyoza-dialog');
-
-
-const reformClose = document.getElementById('reform-close');
 const gyozaClose = document.getElementById('gyoza-close');
 
 
 
+// モーダル開ける
 reformBtn.addEventListener('click', () => {
     reformDialog.showModal();
 })
+
+mypageBtn.addEventListener('click', () => {
+    mypageDialog.showModal();
+})
+
+
 gyozaBtn.addEventListener('click', () => {
     gyozaDialog.showModal();
 })
 
 
-
+// モーダル閉じる
 reformClose.addEventListener('click', () => {
     reformDialog.close();
 })
+mypageClose.addEventListener('click', () => {
+    mypageDialog.close();
+})
+
 gyozaClose.addEventListener('click', () => {
     gyozaDialog.close();
 })
