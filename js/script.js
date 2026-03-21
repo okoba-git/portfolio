4b023c3e6ee8f57ddaa5d41ca7e078132292b933
okoba <taakiwami@gmail.com>
2026-03-21 17:14:17 +0900

    date.js作成

--- js/script.js
+++ js/script.js
@@ -3,76 +3,12 @@ function getID(idName) {
     return document.getElementById(idName);
 }
 
-
-
 // ハンバーガーメニュー
 const humbugerBtn = getID('humburger-btn');
 
 humbugerBtn.addEventListener('click', () => {
     humbugerBtn.classList.toggle("active");
 })
-
-
-
-// 作品情報を配列管理
-
-const worksArray = [
-    {
-        title: 'リカレント工務店',
-        name: 'reform',
-        date: '2026年1月～2月制作',
-        img: 'reform.jpg',
-        realize: ['デザイン', 'レスポンシブ対応', 'ハンバーガーメニュー', 'フィルター機能', '問い合わせフォーム', 'バリデーション', 'Git管理', 'AI活用', 'BEM命名規則', 'CSSアニメーション', 'スクロールアニメーション'],
-        url: 'https://okoba-git.github.io/reform/index.html',
-        git: 'https://github.com/okoba-git/reform',
-        skill: ['HTML', 'CSS', 'JS', '個人制作', '動的表示'],
-        comment: '『地域に寄り添う、親しみやすいリフォーム会社』をテーマに、デザインから実装まで一人で完成させた架空のコーポレートサイトです。'
-    },
-    {
-        title: 'ポートフォリオ',
-        name: 'mypage',
-        date: '2026年3月制作',
-        img: 'mypage.webp',
-        realize: ['デザイン', 'レスポンシブ対応', 'ハンバーガーメニュー', 'CSSアニメーション', 'モーダル', 'Git管理', 'BEM命名規則'],
-        url: 'https://okoba-git.github.io/portfolio/index.html',
-        git: 'https://github.com/okoba-git/portfolio',
-        skill: ['HTML', 'CSS', 'JS', '個人制作', '動的表示'],
-        comment: '初めての自身のポートフォリオサイトです。'
-    },
-    {
-        title: '社員情報一覧',
-        name: 'employees',
-        date: '2026年2月制作',
-        img: 'employees.webp',
-        realize: ['配列管理', 'フィルター機能', 'モーダル'],
-        url: 'https://okoba-git.github.io/emproyees/',
-        git: 'https://github.com/okoba-git/emproyees',
-        skill: ['JS', '学校課題', '動的表示'],
-        comment: '学校のJavaScript試験での制作課題です。HTML・CSSは完成されており、配列管理された情報をJSにて表示させました。'
-    },
-    {
-        title: 'ふくおか餃子FES',
-        name: 'gyoza',
-        date: '2025年12月～1月制作',
-        img: 'gyoza.webp',
-        realize: ['デザイン', 'レスポンシブ対応', 'ハンバーガーメニュー', '問い合わせフォーム', 'Git管理', 'BEM命名規則'],
-        task: ['トップページ下部（コーディング）', 'NEWS（コーディング）', 'CONTACT（デザイン・コーディング）', 'FAQ（デザイン）', 'フッター（デザイン・コーディング）', 'その他'],
-        url: 'https://dwdw0421-blip.github.io/gyoza-fes_a/',
-        git: 'https://github.com/dwdw0421-blip/gyoza-fes_a',
-        skill: ['HTML', 'CSS', 'チーム制作'],
-        comment: '授業で取り組んだグループ（３人）課題です。'
-    },
-    {
-        title: 'Go Out Gear',
-        name: 'go',
-        date: '2025年11月制作',
-        img: 'go.webp',
-        realize: ['レスポンシブ対応'],
-        url: 'https://okoba-git.github.io/gooutgear/',
-        git: 'https://github.com/okoba-git/gooutgear',
-        skill: ['HTML', 'CSS', '学校課題'],
-        comment: '学校のHTML・CSS試験での制作課題です。'
-    }]
 
 
 
