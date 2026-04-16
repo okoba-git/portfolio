f65ff9186befc98bbe8492f2e3342f04d2fdf9af
okoba-git <taakiwami@gmail.com>
2026-04-16 23:57:53 +0900

    モーダル配置変更

--- js/update.js
+++ js/update.js
@@ -24,22 +24,21 @@ worksArray.forEach((work, index) => {
                         ${work.skill.map(item => `<li>${item}</li>`).join('')} 
                         </ul>
                         <p class="c-works__click-btn">Click here！</p>
-                    </button
-                     > `;
+                    </button> `;
     cardArea.insertAdjacentHTML('beforeend', cardHtml);
 })
 
 
 
 
-// 各要素取得
+// モーダルに必要な各要素取得
 // 開閉時に使う
 const dialog = document.getElementById('dialog');
 // close取得
 const close = document.getElementById('close');
 // 矢印の要素取得
-let before = document.querySelector('.before-btn');
-let after = document.querySelector('.after-btn');
+let before = document.getElementById('before-btn');
+let after = document.getElementById('after-btn');
 
 const title = document.getElementById('dialog_title');
 const info = document.querySelector('.l-dialog__info');
@@ -50,24 +49,52 @@ const items = document.getElementById('dialog_items');
 function writeModalFunc(num) {
     title.innerText = worksArray[num].title;
 
-    info.innerHTML = `<p>${worksArray[num].category}</p><p>${worksArray[num].comment}</p>`;
+    info.innerHTML = `<p>【${worksArray[num].category}】</p><p>${worksArray[num].comment}</p>`;
 
     linkArea.innerHTML = `<a href="${worksArray[num].url}"><img src="image/${worksArray[num].img}" alt="" class="p-dialog__img"></a><div class="l-dialog__btns"><a href="${worksArray[num].url}">Go page</a><a href="${worksArray[num].git}"><img src="image/github-icon.svg" alt="">`;
 
     items.innerHTML = worksArray[num].realize.map(item => `<li class="p-dialog__box">${item}</li>`).join('');
 }
 
-// カードをクリック
-cardArea.addEventListener('click', () => {
-    currentNum = cardArea.dataset.cardNum
-    // モーダルを開く
-    dialog.showModal();
-    writeModalFunc(0);
 
+// カードのIDを取得
+// ID名だけの配列を生成
+let worksIdArray = worksArray.map(e => {
+    return `${e.name}-card`;
+});
+// 実際に取得
+let cardId = worksIdArray.map(id => {
+    return document.getElementById(`${id}`);
 })
 
+
+// カードをクリック
+cardId.forEach((e, index) => {
+    e.addEventListener('click', () => {
+    currentNum = index;
+    // モーダルを開く
+    dialog.showModal();
+    writeModalFunc(index);
+})})
 
 // モーダルを閉じる
 close.addEventListener('click', () => {
     dialog.close();
 });
+
+
+before.addEventListener('click',()=>{
+    currentNum--;
+        if (currentNum < 0 || currentNum >= worksArray.length) {
+        currentNum = (worksArray.length - 1)
+    }
+    writeModalFunc(currentNum);
+})
+
+after.addEventListener('click',()=>{
+    currentNum++;
+   if (currentNum < 0 || currentNum >= worksArray.length) {
+        currentNum = 0
+    }
+    writeModalFunc(currentNum);
+})
