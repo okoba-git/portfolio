32ac7c425770f7cd41ba53eeb94333bf118a5c16
okoba-git <taakiwami@gmail.com>
2026-04-16 23:59:49 +0900

    ファイル名変更

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ humbugerBtn.addEventListener('click', () => {
 })
 
 
+// ダイアログ
+let currentNum = 0;
 
 // カード描画エリアの取得（常に表示するもの）
 const cardArea = document.getElementById('work-card-area');
 // foreachで描画
-worksArray.forEach(work => {
+worksArray.forEach((work, index) => {
     let cardHtml = '';
-    cardHtml += `<button class="c-work-card" id="${work.name}-card">
+    cardHtml += `<button class="c-work-card" id="${work.name}-card" data-card-num="${index}">
     <p>【${work.category}】</p>
                         <h3 class="c-sub-title u-notosans">${work.title}</h3>
                         <p class="u-notosans">${work.date}</p>
@@ -22,60 +24,36 @@ worksArray.forEach(work => {
                         ${work.skill.map(item => `<li>${item}</li>`).join('')} 
                         </ul>
                         <p class="c-works__click-btn">Click here！</p>
-                    </button
-                     > `;
+                    </button> `;
     cardArea.insertAdjacentHTML('beforeend', cardHtml);
 })
 
 
-// モーダル描画エリア取得
-// const dialogWriteArea = document.getElementById('dialog-write-area');
 
-const opArea = document.querySelector('.c-dialog__top-area');
-const skillArea = document.querySelector('.c-work__skills');
-const linkArea = document.querySelector('c-dialog__link-area');
 
-// 各要素取得
+// モーダルに必要な各要素取得
 // 開閉時に使う
 const dialog = document.getElementById('dialog');
 // close取得
 const close = document.getElementById('close');
 // 矢印の要素取得
-let before = document.querySelector('.before-btn');
-let after = document.querySelector('.after-btn');
+let before = document.getElementById('before-btn');
+let after = document.getElementById('after-btn');
+
+const title = document.getElementById('dialog_title');
+const info = document.querySelector('.l-dialog__info');
+const linkArea = document.querySelector('.l-dialog__links');
+const items = document.getElementById('dialog_items');
 
 
-// 該当のモーダルのHTML(dialogの中身)
-function modalInner(n) {
+function writeModalFunc(num) {
+    title.innerText = worksArray[num].title;
 
+    info.innerHTML = `<p>【${worksArray[num].category}】</p><p>${worksArray[num].comment}</p>`;
 
-    //     let html = `<div class="c-dialog__top-area--text"><h3 class="c-sub-title u-notosans">${n.title}</h3>
+    linkArea.innerHTML = `<a href="${worksArray[num].url}"><img src="image/${worksArray[num].img}" alt="" class="p-dialog__img"></a><div class="l-dialog__btns"><a href="${worksArray[num].url}">Go page</a><a href="${worksArray[num].git}"><img src="image/github-icon.svg" alt="">`;
 
-
-    //     <img class="c-dialog__img" src="image/${n.img}" alt="${n.title}HPのトップページデザイン画像。">
-
-
-    // ${n.skill.map(item => `<li>${item}</li>`).join('')} 
-    // <ul class="u-notosans c-dialog__realize">
-    // ${n.realize.map(item => `<li>${item}</li>`).join('')}</ul>
-    // <!--タスク項目があれば表示(三項演算子) -->
-    //         ${n.task ? `<div class="c-dialog__tasks"><p>担当ページ</p><ul class="u-center-text u-notosans p-mytasks">${n.task.map(item => ` <li> ${item}</li>`).join('')}</ul></div>` : ''}
-
-
-    // <a href="${n.url}" target="_blank" class="c-dialog__link-text">サイトを見る</a><a href="${n.git}" target="_blank"><img src="image/github-icon.svg" class="c-dialog__link-img" alt=""></a>
-    // <p class="u-center-text u-notosans">${n.comment}</p>`;
-
-    return html;
-}
-
-// 今見ているモーダルのインデックス番号を管理する
-let currentIndex = 0;
-
-// ダイアログタグの中身を作る関数
-function writeModal(indexNum) {
-    currentIndex = indexNum
-    // モーダルに描画
-    dialogWriteArea.innerHTML = modalInner(worksArray[indexNum]);;
+    items.innerHTML = worksArray[num].realize.map(item => `<li class="p-dialog__box">${item}</li>`).join('');
 }
 
 
@@ -93,33 +71,30 @@ let cardId = worksIdArray.map(id => {
 // カードをクリック
 cardId.forEach((e, index) => {
     e.addEventListener('click', () => {
-        // モーダルを開く
-        dialog.showModal();
-        // 開いたモーダルの中身を書く
-        writeModal(index);
-    })
-});
+    currentNum = index;
+    // モーダルを開く
+    dialog.showModal();
+    writeModalFunc(index);
+})})
 
 // モーダルを閉じる
 close.addEventListener('click', () => {
     dialog.close();
-})
+});
 
-// 矢印の操作
-before.addEventListener('click', () => {
-    currentIndex--;
-    if (currentIndex < 0 || currentIndex >= worksArray.length) {
-        currentIndex = (worksArray.length - 1)
+
+before.addEventListener('click',()=>{
+    currentNum--;
+        if (currentNum < 0 || currentNum >= worksArray.length) {
+        currentNum = (worksArray.length - 1)
     }
-    writeModal(currentIndex);
+    writeModalFunc(currentNum);
 })
 
-after.addEventListener('click', () => {
-    currentIndex++;
-    if (currentIndex < 0 || currentIndex >= worksArray.length) {
-        currentIndex = (0)
+after.addEventListener('click',()=>{
+    currentNum++;
+   if (currentNum < 0 || currentNum >= worksArray.length) {
+        currentNum = 0
     }
-    writeModal(currentIndex);
+    writeModalFunc(currentNum);
 })
-
-
